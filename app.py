e55a381689485c88aeec0d623b4712e7700c500f
xiangerwu <wyc793146@gmail.com>
2025-03-18 02:43:53 +0800

    # 強制重定向到 HTTPS 第三次

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ app.secret_key = g_secret_key
 
 
 # 強制重定向到 HTTPS
-# @app.before_request
-# def enforce_https():
-#     # 檢查請求頭 X-Forwarded-Proto 是否為 'https'
-#     if request.headers.get("X-Forwarded-Proto") != "https":
-#         # 如果不是 HTTPS，強制重定向到 HTTPS
-#         return redirect(request.url.replace("http://", "https://", 1))
+@app.before_request
+def enforce_https():
+    # 檢查是否來自 HTTP，使用 X-Forwarded-Proto 標頭
+    if request.headers.get("X-Forwarded-Proto") == "http":
+        # 強制重定向到 HTTPS
+        return redirect(request.url.replace("http://", "https://", 1), code=301)
 
 
 # 首頁
